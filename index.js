7ed90777917a0dc14f04a7961753f630ed00737d
Mario Guimaraes <marioguima@hotmail.com>
2019-07-01 00:15:04 -0300

    feat: add image download logic with fallback

--- index.js
+++ index.js
@@ -6,16 +6,12 @@ const robots = {
 }
 
 async function start() {
-
-	// robots.input()
-	// await robots.text()
+	robots.input()
+	await robots.text()
 	await robots.image()
 	
 	const content = robots.state.load()
-	
 	console.dir(content, { depth: null })
-
-  // console.log(JSON.stringify(content, null, 4))
 }
 
 start()
